1f92a1c6457c93428fe0b8d5664109a798e6f1c3
rasmus <4j534j@gmail.com>
2023-11-13 10:55:50 +0200

    ül 4

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,36 +5,51 @@ import "./Expenses.css"
 import Card from '../UI/Card'
 
 const Expenses = (props) => {
-	const [filteredYear, setFilteredYear] = 
-	useState('2023')
 
-	console.log('Year data in Expenses.js' + filteredYear)
+	const [expenseFilter, setExpenseFilter] = useState('2023')
 
+	const addExpenseFilterHandler = ((expenseFilter) => {
+        console.log('In Expenses.js')
+        console.log(expenseFilter)
+        setExpenseFilter(expenseFilter)
+    })
+
+	
 	const filterChangeHandler = (year) => {
-		console.log('Filter  change handled by Expenses.js')
-		console.log(year, + 'in Expenses.js')
-		setFilteredYear(year)
+		console.log('Filter  change handled by Ex)penses.js')
+		console.log(expenseFilter, + 'in Expenses.js')
+		setExpenseFilter(expenseFilter)
 	}
-	console.log(props.expensesData)
-	props.expensesData.map((expense) => {
-		console.log(expense)
-	})
+	// console.log(props.expensesData)
+	// props.expensesData.map((expense) => {
+	// 	console.log(expense)
+	// })
 
 	return (
-		<Card className="Expenses">
-			<ExpenseFilter selected={filteredYear}
-				onChangeFilter={filterChangeHandler}
-				> </ExpenseFilter>
+		<Card className="expenses">
+            <ExpenseFilter selected={expenseFilter}
+            onChangeFilter={filterChangeHandler}
+            onAddExpenseFilter={addExpenseFilterHandler}
+            ></ExpenseFilter>
 			{
 				props.expensesData.map((expense) => {
-					return <ExpenseItem
-						id={expense.id}
-						title={expense.title}
-						amount={expense.price}
-						date={expense.date}
-					></ExpenseItem>
-				})
-			}
+					
+					if (expense.date.getFullYear().toString() === expenseFilter){
+                        return <ExpenseItem
+            
+                        id={expense.id}
+                        title={expense.title}
+                        amount={expense.price}
+                        date={expense.date}
+                    ></ExpenseItem>  
+                    } 
+                    
+                
+                    else {
+                        return null}
+                    
+                    }) 
+            }             
 		</Card>
 	)
 
